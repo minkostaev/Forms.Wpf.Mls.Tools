41b7e7a9c6ae5670876ac3506b1696aebf0bd3b8
Minko Staev <minkostaev@yahoo.com>
2023-03-12 23:02:25 +0200

    feat: wpf improvements

--- Forms.Wpf.Mls.Tools/Services/AssemblyProperties.cs
+++ Forms.Wpf.Mls.Tools/Services/AssemblyProperties.cs
@@ -4,62 +4,24 @@ namespace Forms.Wpf.Mls.Tools.Services;
 
 public static class AssemblyProperties
 {
-    private static Assembly? AssemblyEntry => Assembly.GetEntryAssembly();
-    private static Assembly? AssemblyExecuting => Assembly.GetExecutingAssembly();
+    private static Assembly? AssemblyEntry => Assembly.GetEntryAssembly();//exe
+    private static Assembly? AssemblyExecuting => Assembly.GetExecutingAssembly();//dll
 
     /// <summary>
     /// Exe name
     /// </summary>
-    public static string AssemblyName
-    {
-        get
-        {
-            var assembly = AssemblyEntry;
-            if (assembly == null)
-                return "AssemblyName";
-            else
-            {
-                var name = assembly.GetName().Name;
-                if (name == null)
-                    return "AssemblyName";
-                else
-                    return name;
-            }
-        }
-    }
-
+    public static string? AssemblyName => AssemblyEntry?.GetName().Name;
+    public static string? DllName => AssemblyExecuting?.GetName().Name;
+    
     /// <summary>
     /// Exe icon
     /// </summary>
-    public static Icon? AssemblyIcon
-    {
-        get
-        {
-            if (AssemblyEntry == null)
-            {
-                return null;
-            }
-            else
-            {
-                return Icon.ExtractAssociatedIcon(AssemblyEntry.Location);
-            }
-        }
-    }
+    public static Icon? AssemblyIcon => 
+        (AssemblyEntry != null) ? Icon.ExtractAssociatedIcon(AssemblyEntry.Location) : null;
+    
 
 
-    public static string? AppVersion
-    {
-        get
-        {
-            try
-            {
-                return AssemblyExecuting?.GetName()?.Version?.ToString();
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-        }
-    }
+    public static string? AppVersion => AssemblyEntry?.GetName()?.Version?.ToString();
+    public static string? DllVersion => AssemblyExecuting?.GetName()?.Version?.ToString();
 
 }
--- Wpf.Test.Helper/MainWindow.xaml.cs
+++ Wpf.Test.Helper/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using Forms.Wpf.Mls.Tools.Services;
-using System.Diagnostics;
 using System;
 using System.Windows;
+using Wpf.Test.Helper.Controls;
 
 namespace Wpf.Test.Helper;
 
@@ -20,34 +20,8 @@ public partial class MainWindow : Window
         // I want to use it in shortcut properties after installation
         string[] args = Environment.GetCommandLineArgs();
 
-        #region Special Folders
-        tbStartSystem.Text = SpecialFolders.StartupSystem;
-        tbStartUser.Text = SpecialFolders.StartupUser;
-
-        tbUser.Text = Environment.UserName;
-        tbLocal.Text = SpecialFolders.LocalAppData;
-
-        btnStartupSystem.Click += delegate { Process.Start("explorer.exe", tbStartSystem.Text); };
-        btnStartupUser.Click += delegate { Process.Start("explorer.exe", tbStartUser.Text); };
-        #endregion
-
-        #region System Tray
-        //var SystemIcon = new SystemTray(this);
-        //chBxSystemTray.IsChecked = true;
-        //chBxSystemTray.Checked += delegate { SystemIcon.Visibility = true; };
-        //chBxSystemTray.Unchecked += delegate { SystemIcon.Visibility = false; };
-        #endregion
-
-        #region Always On Top
-        chBxAlwaysOnTop.Checked += delegate { this.Topmost = true; };
-        chBxAlwaysOnTop.Unchecked += delegate { this.Topmost = false; };
-        #endregion
-
-        #region Show In Taskbar
-        chBxShowInTaskbar.IsChecked = true;
-        chBxShowInTaskbar.Checked += delegate { this.ShowInTaskbar = true; };
-        chBxShowInTaskbar.Unchecked += delegate { this.ShowInTaskbar = false; };
-        #endregion
+        grdFolders.Children.Add(new Folders());
+        grdWindows.Children.Add(new Windows(this));
 
     }
 
