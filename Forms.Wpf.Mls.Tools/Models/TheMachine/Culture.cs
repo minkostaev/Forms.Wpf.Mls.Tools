8185fadd809d05917a935eed65d4507e62500180
Minko Staev <minkostaev@yahoo.com>
2024-03-25 23:00:14 +0200

    feat: the machines

---
+++ Forms.Wpf.Mls.Tools/Models/TheMachine/Culture.cs
@@ -0,0 +1,21 @@
+﻿namespace Forms.Wpf.Mls.Tools.Models.TheMachine;
+
+using System.Globalization;
+
+public class Culture
+{
+    public Culture()
+    {
+        try
+        {
+            CultureName = CultureInfo.CurrentCulture.Name;
+            EnglishName = CultureInfo.CurrentCulture.EnglishName;
+            DecimalSeparator = CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator;
+        }
+        catch (Exception) { }
+    }
+
+    public string? CultureName { get; private set; }
+    public string? EnglishName { get; private set; }
+    public string? DecimalSeparator { get; private set; }
+}
